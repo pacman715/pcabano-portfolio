c7a4eb6cd6b65730b3114bffa69ca0edc295256e
pacman715 <54875134+pacman715@users.noreply.github.com>
2019-10-15 21:06:31 -0700

    Delete Comma_Code.py

--- Week 4/Comma_Code.py
+++
@@ -1,8 +0,0 @@
-spam = ['apples', 'pizza', 'dogs', 'cats']
-
-def grammar(words):
-    for i in range(len(words) -2):
-        print(words[i], end = ", ")
-    print(words[-2] + ', and ' + words[-1]) 
-
-grammar(spam)
